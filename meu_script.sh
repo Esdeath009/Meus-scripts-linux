06911e85d2cf7ffd6c08ea382ccbfb2b4dca0437
Esdeath009 <reptorgamer009@gmail.com>
2026-05-28 13:57:48 -0300

    Meu primeiro script de automacao Bash pelo celular

---
+++ meu_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Iniciando a automacao pelo celular..."
+for numero in {1..5}
+do
+    mkdir "projeto-celular-$numero"
+    echo "Pasta projeto-celular-$numero criada!"
+done
+echo "Automacao concluida com sucesso!"
